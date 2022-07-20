cd9e6e7f35887199056ad9282e8f9046c9419cc2
Mehmet Emin Eker <mehmetemineker92@gmail.com>
2022-07-20 12:05:04 +0300

    seeddata

---
+++ backend/Soltree.Api/Soltree.Api/Data/SeedData.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Soltree.Api.Data
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using var dbContext = new AppDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<AppDbContext>>());
+
+            PopulateTestData(dbContext);
+        }
+
+        public static void PopulateTestData(AppDbContext dbContext)
+        {
+            if (dbContext.Brands.Any())
+            {
+                return;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                dbContext.Brands.Add(new Etities.Brand($"Brand - {i + 1}")
+                {
+                    Id = Guid.NewGuid()
+                });
+            }
+
+            dbContext.SaveChanges();
+        }
+    }
+}
--- backend/Soltree.Api/Soltree.Api/Program.cs
+++ backend/Soltree.Api/Soltree.Api/Program.cs
@@ -43,4 +43,24 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+
+// Seed Database
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        context.Database.Migrate();
+        context.Database.EnsureCreated();
+        SeedData.Initialize(services);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred seeding the DB.");
+    }
+}
+
 app.Run();
